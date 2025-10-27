333088c1f60ea07018a4c6e8020a18bc8dccffbe
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-10-27 21:29:56 +0900

    [chore] pre-commit 임시 테스트

--- qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
+++ qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
@@ -3,20 +3,21 @@ package com.beyond.qiin.common.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
-
-
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 @OpenAPIDefinition(
         info = @Info(title = "Queuein Service API Test", description = "회사 내 자원 예약 일정 관리 시스템", version = "v1.0.0"))
 public class SwaggerConfig {
