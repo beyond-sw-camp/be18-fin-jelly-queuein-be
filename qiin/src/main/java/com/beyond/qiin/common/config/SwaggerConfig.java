c43e664dd679e972c0669173626601ef8a949885
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-10-27 21:18:09 +0900

    [chore] pre-commit 테스트 commit 시 확인

--- qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
+++ qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
