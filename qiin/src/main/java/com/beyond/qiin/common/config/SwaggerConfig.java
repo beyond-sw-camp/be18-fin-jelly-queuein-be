d238087050b269a0e93cb691bbb5b35d754db3f7
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-10-27 21:45:15 +0900

    [chore] pre-commit 임시 테스트

--- qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
+++ qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
