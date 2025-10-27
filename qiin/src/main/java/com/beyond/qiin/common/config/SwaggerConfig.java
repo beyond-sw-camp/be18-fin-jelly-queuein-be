08f45a56fa33df462cabdf4cd2b13e6f630b5c09
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-10-27 21:54:00 +0900

    [chore] pre-commit .githook 위치 찾음

--- qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
+++ qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
@@ -4,9 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
