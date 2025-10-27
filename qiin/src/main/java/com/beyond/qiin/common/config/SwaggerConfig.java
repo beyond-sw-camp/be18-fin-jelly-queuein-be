314056ffb37c14394e48aecf7a61a517963311d2
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-10-27 21:14:35 +0900

    [chore] pre-commit 테스트 exec 변경

--- qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
+++ qiin/src/main/java/com/beyond/qiin/common/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.beyond.qiin.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
