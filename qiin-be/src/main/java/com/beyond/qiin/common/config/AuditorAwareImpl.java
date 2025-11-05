34301df16244b324ab078a740ebd7e2385893111
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2025-11-05 12:07:51 +0900

    [REFACTOR] BaseEntity ID 추가 및 audit 추가 (#16)
    * [feat] spotless
    
    * [feat] BaseEntity Audit 추가
    
    * [feat] dockerfile 이미지 테무린으로 수정

---
+++ qiin-be/src/main/java/com/beyond/qiin/common/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package com.beyond.qiin.common.config;
+
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    @NonNull
+    public Optional<Long> getCurrentAuditor() {
+        // TODO: 추후 인증 로직 구현 시 Security 연동
+
+        return Optional.of(0L);
+    }
+}
