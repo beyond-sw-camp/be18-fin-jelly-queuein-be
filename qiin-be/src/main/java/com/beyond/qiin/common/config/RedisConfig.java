8742ebf713559c8e6db2dc89f4a0ba7930cb1622
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-11-10 21:27:40 +0900

    [feat] redis 연결 실패 spotless 후

--- qiin-be/src/main/java/com/beyond/qiin/common/config/RedisConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/common/config/RedisConfig.java
@@ -10,31 +10,29 @@ import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-
 @Slf4j
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
-  @Value("${REDIS_HOST}")
-  private String host;
+    @Value("${REDIS_HOST}")
+    private String host;
 
-  @Value("${REDIS_PORT}")
-  private int port;
+    @Value("${REDIS_PORT}")
+    private int port;
 
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(host, port);
-  }
-
-  @PostConstruct
-  public void verifyRedisConnection() {
-    try (RedisConnection connection = redisConnectionFactory().getConnection()) {
-      String pong = connection.ping();
-      log.info("Redis 연결 성공: {}:{}, 응답 = {}", host, port, pong);
-    } catch (Exception ex) {
-      log.error("Redis 연결 실패: {}:{}, 에러={}", host, port, ex.getMessage());
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
     }
-  }
 
+    @PostConstruct
+    public void verifyRedisConnection() {
+        try (RedisConnection connection = redisConnectionFactory().getConnection()) {
+            String pong = connection.ping();
+            log.info("Redis 연결 성공: {}:{}, 응답 = {}", host, port, pong);
+        } catch (Exception ex) {
+            log.error("Redis 연결 실패: {}:{}, 에러={}", host, port, ex.getMessage());
+        }
+    }
 }
