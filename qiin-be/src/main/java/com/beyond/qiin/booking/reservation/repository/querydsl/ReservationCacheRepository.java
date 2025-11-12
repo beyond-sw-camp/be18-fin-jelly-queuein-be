4267c403714a229c1da88b816466213a87340062
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-12 15:45:07 +0900

    [wip] kafka

--- qiin-be/src/main/java/com/beyond/qiin/booking/reservation/repository/querydsl/ReservationCacheRepository.java
+++ qiin-be/src/main/java/com/beyond/qiin/booking/reservation/repository/querydsl/ReservationCacheRepository.java
@@ -1,8 +1,8 @@
-//package com.beyond.qiin.booking.reservation.repository.querydsl;
+// package com.beyond.qiin.booking.reservation.repository.querydsl;
 //
-//import com.beyond.qiin.booking.reservation.entity.Reservation;
-//import org.springframework.data.repository.CrudRepository;
+// import com.beyond.qiin.booking.reservation.entity.Reservation;
+// import org.springframework.data.repository.CrudRepository;
 //
-//public interface ReservationCacheRepository extends CrudRepository<Reservation, Long> {
+// public interface ReservationCacheRepository extends CrudRepository<Reservation, Long> {
 //
-//}
+// }
