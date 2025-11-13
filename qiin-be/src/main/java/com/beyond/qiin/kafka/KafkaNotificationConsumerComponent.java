858c818bdb601c56ca2591467e5013ac026c40c4
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-13 17:17:13 +0900

    [fix] application.yml group id 제거

--- qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaNotificationConsumerComponent.java
+++ qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaNotificationConsumerComponent.java
@@ -10,18 +10,18 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class KafkaNotificationConsumerComponent {
-  private final KafkaNotificationConsumerService kafkaNotificationConsumerService;
+    private final KafkaNotificationConsumerService kafkaNotificationConsumerService;
 
-  @KafkaListener(topics = "#{@kafkaTopicProperties.getNotification()}", groupId = "notification-group")
-  public void listen(String message) {
-    try {
-      log.info("메시지 수신: {}", message);
-      // 비즈니스 로직 처리
-      String processedMessage = kafkaNotificationConsumerService.processMessage(message);
-      log.info("메시지 처리 완료: {}", processedMessage);
+    @KafkaListener(topics = "#{@kafkaTopicProperties.getNotification()}", groupId = "notification-group")
+    public void listen(String message) {
+        try {
+            log.info("메시지 수신: {}", message);
+            // 비즈니스 로직 처리
+            String processedMessage = kafkaNotificationConsumerService.processMessage(message);
+            log.info("메시지 처리 완료: {}", processedMessage);
 
-    } catch (Exception e) {
-      log.error("메시지 처리 중 오류 발생: {}", e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("메시지 처리 중 오류 발생: {}", e.getMessage(), e);
+        }
     }
-  }
 }
--- qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaTopicProperties.java
+++ qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaTopicProperties.java
@@ -1,7 +1,6 @@
 package com.beyond.qiin.kafka;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
