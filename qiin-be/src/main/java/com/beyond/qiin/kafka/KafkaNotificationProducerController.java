029256bc71d7f22787382a77cc6a3d0034a610f2
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-13 17:34:45 +0900

    [fix] restcontroller url 제거

--- qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaNotificationProducerController.java
+++ qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaNotificationProducerController.java
@@ -6,12 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/api/v1/kafka/notifications")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/kafka/notifications")
 public class KafkaNotificationProducerController {
     private final KafkaNotificationProducerService kafkaNotificationProducerService;
 
--- qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaReservationProducerController.java
+++ qiin-be/src/main/java/com/beyond/qiin/kafka/KafkaReservationProducerController.java
@@ -1,14 +1,17 @@
 package com.beyond.qiin.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/api/v1/kafka/reservations")
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/kafka/reservations")
 @RequiredArgsConstructor
 public class KafkaReservationProducerController {
     private final KafkaReservationProducerService kafkaReservationProducerService;
