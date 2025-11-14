ce24fd94f5be68c70f31c53428717a7f0e7efa0c
promandu <clsrn2376@naver.com>
2025-11-14 12:44:24 +0900

    [bug] RedisConfig 수정 (#65)

--- qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -16,6 +17,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Slf4j
 @Configuration
 @EnableCaching
+@Profile("!test")
 public class RedisConfig {
 
     @Value("${REDIS_HOST}")
