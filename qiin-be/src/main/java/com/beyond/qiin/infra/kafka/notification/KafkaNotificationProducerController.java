3aaaca8e2ea21c2451148986fd4325963e21fd8a
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-14 16:10:53 +0900

    [fix] kafka 경로 수정

--- qiin-be/src/main/java/com/beyond/qiin/infra/kafka/notification/KafkaNotificationProducerController.java
+++ qiin-be/src/main/java/com/beyond/qiin/infra/kafka/notification/KafkaNotificationProducerController.java
@@ -1,6 +1,5 @@
 package com.beyond.qiin.infra.kafka.notification;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
