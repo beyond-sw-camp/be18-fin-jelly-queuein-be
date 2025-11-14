e64d50474bf26aa7055b1e0da26af51f31b9834e
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-14 12:19:53 +0900

    [wip] kafka tmpfs

--- qiin-be/src/main/java/com/beyond/qiin/infra/kafka/KafkaTopicProperties.java
+++ qiin-be/src/main/java/com/beyond/qiin/infra/kafka/KafkaTopicProperties.java
@@ -1,16 +1,17 @@
 package com.beyond.qiin.infra.kafka;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 // yml별 topic의 key에 따른 value값을 하드코딩하지 않기 위한 용도
 
-@Configuration
+//@RequiredArgsConstructor
+@Component
 @ConfigurationProperties(prefix = "spring.kafka.topic")
 @Getter
-@Setter
 public class KafkaTopicProperties {
+
     private String reservation;
     private String notification;
 }
