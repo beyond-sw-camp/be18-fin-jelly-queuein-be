7eaa018ccf5ed4c2f5ab032f8869e3e5c9376a36
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-14 17:06:56 +0900

    Feature/53 kafka (#58)
    * [wip] kafka
    
    * [wip] kafka
    
    * [wip] kafka
    
    * [wip] kafka
    
    * [wip] yml별 topic 추가
    
    * [wip] yml별 topic 추가
    
    * [wip] yml 들여쓰기 수정
    
    * [wip] change name of consumer related classes
    
    * [wip] change name of consumer related classes
    
    * [wip] kafka yml duplicate
    
    * [wip] kafka yml duplicate
    
    * [fix] application.yml group id 제거
    
    * [fix] application.yml group id 제거
    
    * [fix] restcontroller url 제거
    
    * [fix] restcontroller url 제거
    
    * [feat] kafka ui
    
    * [feat] kafka ui
    
    * [refactor] infra folder
    
    * [fix] kafka server env화
    
    * [wip] kafka tmpfs
    
    * [wip] kafka annotations
    
    * [fix] setter x
    
    * [fix] setter x
    
    * [refactor] 주석 제거
    
    * [fix] kafka test yml
    
    * [fix] kafka 경로 수정
    
    * [fix] kafka 경로 수정
    
    * [docs] readme img

---
+++ qiin-be/src/main/java/com/beyond/qiin/infra/kafka/reservation/KafkaReservationConsumerComponent.java
@@ -0,0 +1,27 @@
+package com.beyond.qiin.infra.kafka.reservation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+// 메시지 수신용
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaReservationConsumerComponent {
+    private final KafkaReservationConsumerService kafkaReservationConsumerService;
+
+    @KafkaListener(topics = "${spring.kafka.topic.reservation}", groupId = "reservation-group")
+    public void listen(String message) {
+        try {
+            log.info("메시지 수신: {}", message);
+            // 비즈니스 로직 처리
+            String processedMessage = kafkaReservationConsumerService.processMessage(message);
+            log.info("메시지 처리 완료: {}", processedMessage);
+
+        } catch (Exception e) {
+            log.error("메시지 처리 중 오류 발생: {}", e.getMessage(), e);
+        }
+    }
+}
