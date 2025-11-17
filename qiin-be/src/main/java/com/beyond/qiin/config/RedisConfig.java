9120960bdeb655366a76d1a502c4f36c69fb746b
promandu <clsrn2376@naver.com>
2025-11-17 18:08:16 +0900

    [FEAT] redisconfig dev환경에서만 활성화

--- qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
@@ -17,7 +17,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Slf4j
 @Configuration
 @EnableCaching
-@Profile("!test")
+@Profile("dev")
 public class RedisConfig {
 
     @Value("${REDIS_HOST}")
