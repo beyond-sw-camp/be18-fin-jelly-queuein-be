ebbf47a8bdff751ae035537290f47b9d226ed295
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-20 12:36:12 +0900

    [refactor] user rev as attendant

--- qiin-be/src/main/resources/db/migration/V6__add_attendant_delete_asset_history.sql
+++ qiin-be/src/main/resources/db/migration/V6__add_attendant_delete_asset_history.sql
@@ -6,6 +6,12 @@ CREATE TABLE `attendant`
     `attendant_id`    BIGINT(20)	NOT NULL	COMMENT '참여자 PK',
     `reservation_id` BIGINT(20)	NOT NULL	COMMENT '예약 FK',
     `user_id`        BIGINT(20)	NOT NULL	COMMENT '사용자 FK',
+    `created_at`  TIMESTAMP(6) NOT NULL COMMENT '생성 시각',
+    `created_by`  BIGINT(20)	NOT NULL	COMMENT '생성자',
+    `updated_at`  TIMESTAMP(6) NULL	COMMENT '수정 시각',
+    `updated_by`  BIGINT(20)	NULL	COMMENT '수정자',
+    `deleted_at`  TIMESTAMP(6) NULL	COMMENT '삭제 시각',
+    `deleted_by`  BIGINT(20)	NULL	COMMENT '삭제자',
     PRIMARY KEY (attendant_id)
 );
 
