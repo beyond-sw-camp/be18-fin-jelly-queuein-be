9514222a1f2f5ea95b590c20156a7ebbfd73f849
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-20 17:33:58 +0900

    [fix] service impl 인자

--- qiin-be/src/main/java/com/beyond/qiin/domain/booking/reservation/service/query/ReservationQueryService.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/booking/reservation/service/query/ReservationQueryService.java
@@ -1,41 +1,48 @@
 package com.beyond.qiin.domain.booking.reservation.service.query;
 
+import com.beyond.qiin.common.dto.PageResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.AssetTimeResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.GetAppliedReservationListResponseDto;
+import com.beyond.qiin.domain.booking.dto.reservation.response.GetAppliedReservationResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.GetUserReservationListResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.MonthReservationListResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.ReservableAssetListResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.ReservationDetailResponseDto;
 import com.beyond.qiin.domain.booking.dto.reservation.response.WeekReservationListResponseDto;
 import com.beyond.qiin.domain.booking.reservation.entity.Reservation;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface ReservationQueryService {
     Reservation getReservationById(final Long id);
 
     ReservationDetailResponseDto getReservation(final Long userId, final Long reservationId);
 
-    GetUserReservationListResponseDto getReservationsByUserId(final Long userId, final LocalDate date);
+    GetUserReservationListResponseDto getReservationsByUserId(final Long userId, final LocalDate date, final Pageable pageable);
 
-    ReservableAssetListResponseDto getReservableAssets(final Long userId, final LocalDate date);
+    ReservableAssetListResponseDto getReservableAssets(final Long userId, final LocalDate date,  Pageable pageable);
 
     AssetTimeResponseDto getAssetTimes(final Long userId, final Long assetId, final LocalDate date);
 
-    WeekReservationListResponseDto getWeeklyReservations(final Long userId, final LocalDate date);
+    WeekReservationListResponseDto getWeeklyReservations(
+        final Long userId, Instant start, Instant end) ;// 해당 주의 기준날짜
 
-    MonthReservationListResponseDto getMonthlyReservations(final Long userId, final YearMonth yearMonth);
+    MonthReservationListResponseDto getMonthlyReservations(
+        final Long userId, final Instant from, final Instant to);
 
-    GetAppliedReservationListResponseDto getReservationApplies(final Long userId, final LocalDate date);
+    PageResponseDto<GetAppliedReservationResponseDto> getReservationApplies(
+        final Long userId, final LocalDate date, Pageable pageable);
 
-    List<Reservation> getReservationsByUserAndDate(final Long userId, final LocalDate date);
+    List<Reservation> getReservationsByUserAndDate(final Long userId, final LocalDate date, final Pageable pageable);
 
     List<Reservation> getReservationsByAssetAndDate(final Long userId, final Long assetId, final LocalDate date);
 
-    List<Reservation> getReservationsByUserAndYearMonth(final Long userId, final YearMonth yearMonth);
+    List<Reservation> getReservationsByUserAndYearMonth(final Long userId, final Instant start, final Instant end);
 
-    List<Reservation> getReservationsPendingAndDate(final LocalDate date);
+    List<Reservation> getReservationsPendingAndDate(final Long userId, final LocalDate date, Pageable pageabl);
 
     List<Reservation> getReservationsByAssetId(final Long assetId);
 }
