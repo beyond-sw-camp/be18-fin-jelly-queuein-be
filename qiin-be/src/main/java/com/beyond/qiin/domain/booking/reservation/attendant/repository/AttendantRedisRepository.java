05d2d52112c7776ce16dfb0f883880e6345a7001
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-21 23:55:37 +0900

    [fix] mapping

--- qiin-be/src/main/java/com/beyond/qiin/domain/booking/reservation/attendant/repository/AttendantRedisRepository.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/booking/reservation/attendant/repository/AttendantRedisRepository.java
@@ -3,4 +3,4 @@ package com.beyond.qiin.domain.booking.reservation.attendant.repository;
 import com.beyond.qiin.domain.booking.reservation.attendant.entity.Attendant;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ReservationRedisRepository extends CrudRepository<Attendant, Long> {}
+public interface AttendantRedisRepository extends CrudRepository<Attendant, Long> {}
