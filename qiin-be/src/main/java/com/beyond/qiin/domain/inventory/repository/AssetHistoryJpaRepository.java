5550afa7f40264951c2b548fba1f8c52abe4eeed
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-21 16:57:10 +0900

    [fix] asset history 제거

--- qiin-be/src/main/java/com/beyond/qiin/domain/inventory/repository/AssetHistoryJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.beyond.qiin.domain.inventory.repository;
-
-import com.beyond.qiin.domain.inventory.entity.AssetHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssetHistoryJpaRepository extends JpaRepository<AssetHistory, Long> {}
