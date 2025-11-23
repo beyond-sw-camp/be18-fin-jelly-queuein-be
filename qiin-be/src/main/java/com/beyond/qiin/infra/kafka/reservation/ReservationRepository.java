80c4e356358cda7e29e77332e1ab9cd7f62bd33e
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-23 16:56:15 +0900

    [fix] 불필요 파일 삭제

--- qiin-be/src/main/java/com/beyond/qiin/infra/kafka/reservation/ReservationRepository.java
+++
@@ -1,6 +0,0 @@
-// package com.beyond.qiin.infra.kafka.reservation;
-//
-// import org.springframework.data.jpa.repository.JpaRepository;
-//
-// public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-// }
