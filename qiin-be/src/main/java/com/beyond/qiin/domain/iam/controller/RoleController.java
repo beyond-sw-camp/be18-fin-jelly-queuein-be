105eaa1e44ec34b67749a8e46cca0b8011b55575
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-11-23 23:11:28 +0900

    [feat] Role GET 컨트롤러 작성

--- qiin-be/src/main/java/com/beyond/qiin/domain/iam/controller/RoleController.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/iam/controller/RoleController.java
@@ -2,6 +2,7 @@ package com.beyond.qiin.domain.iam.controller;
 
 import com.beyond.qiin.domain.iam.dto.role.request.CreateRoleRequestDto;
 import com.beyond.qiin.domain.iam.dto.role.request.UpdateRoleRequestDto;
+import com.beyond.qiin.domain.iam.dto.role.response.RoleListResponseDto;
 import com.beyond.qiin.domain.iam.dto.role.response.RoleResponseDto;
 import com.beyond.qiin.domain.iam.service.command.RoleCommandService;
 import com.beyond.qiin.domain.iam.service.query.RoleQueryService;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +32,20 @@ public class RoleController {
     @PreAuthorize("hasAuthority('MASTER')")
     public ResponseEntity<RoleResponseDto> createRole(@Valid @RequestBody final CreateRoleRequestDto request) {
         return ResponseEntity.ok(roleCommandService.createRole(request));
+    }
+
+    // 역할 목록 조회
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('MASTER','ADMIN')")
+    public ResponseEntity<RoleListResponseDto> getRoleList() {
+        return ResponseEntity.ok(roleQueryService.getRoles());
+    }
+
+    // 역할 상세 조회
+    @GetMapping("/{roleId}")
+    @PreAuthorize("hasAnyAuthority('MASTER','ADMIN')")
+    public ResponseEntity<RoleResponseDto> getRole(@PathVariable final Long roleId) {
+        return ResponseEntity.ok(roleQueryService.getRole(roleId));
     }
 
     // REVIEW: PUT 유지하고 2개의 필드 무조건 보내게 하는거 프론트에서 검증하게 하며,
--- qiin-be/src/main/java/com/beyond/qiin/domain/iam/dto/role/response/RoleResponseDto.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/iam/dto/role/response/RoleResponseDto.java
@@ -14,6 +14,10 @@ public class RoleResponseDto {
     private final String roleDescription;
     private final String roleName;
 
+    // TODO: 해당 역할 몇명, 권한 목록 조회 가능
+    //    private final int userCount;
+    //    private final List<String> permissions;
+
     public static RoleResponseDto fromEntity(final Role role) {
         return RoleResponseDto.builder()
                 .roleId(role.getId())
