c23e5825ed9d77b04f0d2d422910179a57bb5ed2
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-25 15:06:23 +0900

    [fix] test config

--- qiin-be/src/test/java/com/beyond/qiin/TestRedisConfig.java
+++ qiin-be/src/test/java/com/beyond/qiin/TestRedisConfig.java
@@ -4,10 +4,11 @@ import org.mockito.Mockito;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
 @TestConfiguration
 public class TestRedisConfig {
-  @Bean
-  public RedissonClient redissonClient() {
-    return Mockito.mock(RedissonClient.class);
-  }
+    @Bean
+    public RedissonClient redissonClient() {
+        return Mockito.mock(RedissonClient.class);
+    }
 }
