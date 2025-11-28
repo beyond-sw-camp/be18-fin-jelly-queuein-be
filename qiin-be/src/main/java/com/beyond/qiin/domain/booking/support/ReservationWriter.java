18bf1b5e5f255e9a68fb8f377813a8232261cfd6
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-11-28 21:45:03 +0900

    [fix] method 명 수정

--- qiin-be/src/main/java/com/beyond/qiin/domain/booking/support/ReservationWriter.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/booking/support/ReservationWriter.java
@@ -22,6 +22,6 @@ public class ReservationWriter {
     }
 
     public List<Reservation> findFutureUsableReservations(Long assetId) {
-        return reservationJpaRepository.findFutureUsableReservationsByUserAndAsset(assetId);
+        return reservationJpaRepository.findFutureUsableReservationsByAsset(assetId);
     }
 }
