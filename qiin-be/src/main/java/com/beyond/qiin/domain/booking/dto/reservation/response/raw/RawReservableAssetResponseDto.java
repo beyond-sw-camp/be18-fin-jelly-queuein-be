992f6aeaa374a25e53dfc3de71d05e0ec563aef8
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-12-01 14:54:09 +0900

    Feature/125 outbox (#133)
    * [feat] outbox
    
    * [feat] reservation event publisher(kafka producer로 send
    
    * [feat] reservation event publisher(kafka producer로 send
    
    * [fix] 불필요 파일 제거
    
    * [fix] payload id
    
    * [fix] payload id
    
    * [fix] payload field Boolean
    
    * [wip] 승인 시 test 중
    
    * [refactor] dto 구조
    
    * [refactor] dto 구조
    
    * [fix] 예약 가능 시간대
    
    * [fix] 예약 가능 시간대
    
    * [fix] 예약 가능 시간대
    
    * [fix] 예약 가능 자원 판별
    
    * [fix] 예약 가능 자원 판별
    
    * [fix] version 값 목록에서 넘겨주기
    
    * [fix] response dto respondent name
    
    * [fix] response dto respondent name
    
    * [fix] 가능 시간 확인 시 자신 제외
    
    * [fix] 가능 시간 확인 시 자신 제외
    
    * [fix] event 인식 안됨
    
    * [fix] event 인식 안됨

--- qiin-be/src/main/java/com/beyond/qiin/domain/booking/dto/reservation/response/raw/RawReservableAssetResponseDto.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/booking/dto/reservation/response/raw/RawReservableAssetResponseDto.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 // TODO : asset type, asset status enum화
 public class RawReservableAssetResponseDto {
     // TODO : 순서대로 넣어줘야함 QUERYDSL PROJECTION 용이라
-    //  private final AssetType assetType;
 
     private final Long assetId;
     private final String assetName;
