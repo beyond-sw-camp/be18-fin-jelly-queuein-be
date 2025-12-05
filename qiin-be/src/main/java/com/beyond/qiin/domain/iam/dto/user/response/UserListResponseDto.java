d7c923bd3316bae51d8f00086b5ab92e2a437e30
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-12-05 11:12:38 +0900

    [feat] 마이페이지 연락처, 생년월일 추가

--- qiin-be/src/main/java/com/beyond/qiin/domain/iam/dto/user/response/UserListResponseDto.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/iam/dto/user/response/UserListResponseDto.java
@@ -10,26 +10,25 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class UserListResponseDto {
-  private final Long userId;
-  private final String userName;
-  private final String email;
-  private final Long dptId;
-  private final String roleName;
-  private final String phone;
-  private final Instant createdAt;
-  private final Instant lastLoginAt;
+    private final Long userId;
+    private final String userName;
+    private final String email;
+    private final Long dptId;
+    private final String roleName;
+    private final String phone;
+    private final Instant createdAt;
+    private final Instant lastLoginAt;
 
-  public static UserListResponseDto fromRaw(final RawUserListResponseDto raw) {
-    return UserListResponseDto
-        .builder()
-            .userId(raw.getUserId())
-            .userName(raw.getUserName())
-            .email(raw.getEmail())
-            .dptId(raw.getDptId())
-            .roleName(raw.getRoleName())
-            .phone(raw.getPhone())
-            .createdAt(raw.getCreatedAt())
-            .lastLoginAt(raw.getLastLoginAt())
-        .build();
-  }
+    public static UserListResponseDto fromRaw(final RawUserListResponseDto raw) {
+        return UserListResponseDto.builder()
+                .userId(raw.getUserId())
+                .userName(raw.getUserName())
+                .email(raw.getEmail())
+                .dptId(raw.getDptId())
+                .roleName(raw.getRoleName())
+                .phone(raw.getPhone())
+                .createdAt(raw.getCreatedAt())
+                .lastLoginAt(raw.getLastLoginAt())
+                .build();
+    }
 }
