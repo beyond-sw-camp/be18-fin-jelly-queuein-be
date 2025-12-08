2dac06e2656afc04b7e10998b83e37ecc2e09581
promandu <clsrn2376@naver.com>
2025-12-08 14:12:50 +0900

    develop 최신화

--- qiin-be/src/main/java/com/beyond/qiin/config/CorsConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/config/CorsConfig.java
@@ -1,11 +1,11 @@
-//package com.beyond.qiin.config;
+// package com.beyond.qiin.config;
 //
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
 //
 //    @Override
 //    public void addCorsMappings(final CorsRegistry registry) {
@@ -24,4 +24,4 @@
 //                .allowedHeaders("*") // 모든 헤더 허용
 //                .allowCredentials(true); // 쿠키/인증 정보(Credentials) 전송 허용
 //    }
-//}
+// }
