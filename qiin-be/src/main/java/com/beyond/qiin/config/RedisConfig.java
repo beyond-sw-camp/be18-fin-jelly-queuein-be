17a9e82916a12e1a12194c6ebb2b66fe1af04adb
promandu <clsrn2376@naver.com>
2025-12-10 19:09:31 +0900

    [FEAT] redis,redissonConfig Profile 제거

--- qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
