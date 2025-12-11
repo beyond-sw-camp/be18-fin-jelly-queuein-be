7424f1f6a3761596b3d7bd7fd6d08564057e821d
promandu <clsrn2376@naver.com>
2025-12-11 10:28:52 +0900

    [FEAT] redis Profile 설정

--- qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/config/RedisConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
+@Profile({"dev", "prod"})
 @Configuration
 public class RedisConfig {
 
